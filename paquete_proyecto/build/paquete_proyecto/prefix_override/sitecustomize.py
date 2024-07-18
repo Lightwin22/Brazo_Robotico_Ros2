972ccf8e1765acd09d37a0e4dbd7f65ff6f46240
Darwin <dmunoz1a14@gmail.com>
2024-07-17 23:13:13 -0400

    Actualizacion final

--- paquete_proyecto/build/paquete_proyecto/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vboxuser/Escritorio/ROS/entorno/paquete_proyecto/install/paquete_proyecto'
